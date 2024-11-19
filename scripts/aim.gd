a46b81a98eabc61817fdc7b160c8eb5d3ae3f89b
Joseph Ertman <ertmanj1@hawkmail.newpaltz.edu>
2024-11-19 17:12:55 -0500

    Phase 2

--- scripts/aim.gd
+++ scripts/aim.gd
@@ -3,8 +3,10 @@ extends Area2D
 var player
 var oryx
 var has_executed: bool = false
+var can_shoot: bool = false
 @onready var attack_timer: Timer = $AttackTimer
 @onready var attack_timer2: Timer = $AttackTimer2
+@onready var attack_timer3: Timer = $AttackTimer3
 
 func _ready():
 	player = get_node("/root/Game/Player")
@@ -20,7 +22,6 @@ func _physics_process(delta: float) -> void:
 func shoot():
 	const SLASH = preload("res://scenes/slash.tscn")
 	const SLOW = preload("res://scenes/slow.tscn")
-	const PARALYZE = preload("res://scenes/paralyze.tscn")
 	var new_slash = SLASH.instantiate()
 	var new_slash2 = SLASH.instantiate()
 	var new_slash3 = SLASH.instantiate()
@@ -43,58 +44,163 @@ func shoot():
 			attack_timer.queue_free()
 			attack_timer2.start()
 			has_executed=true
-		var new_paralyze = PARALYZE.instantiate()
-		var new_paralyze2 = PARALYZE.instantiate()
-		var new_paralyze3 = PARALYZE.instantiate()
-		var new_paralyze4 = PARALYZE.instantiate()
-		var new_paralyze5 = PARALYZE.instantiate()
-		var new_paralyze6 = PARALYZE.instantiate()
-		var new_paralyze7 = PARALYZE.instantiate()
-		var new_paralyze8 = PARALYZE.instantiate()
-		var new_paralyze9 = PARALYZE.instantiate()
-		var new_paralyze10 = PARALYZE.instantiate()
-		var new_paralyze11 = PARALYZE.instantiate()
-		var new_paralyze12 = PARALYZE.instantiate()
-		new_paralyze.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position 
-		new_paralyze2.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
-		new_paralyze3.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
-		new_paralyze4.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
-		new_paralyze5.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
-		new_paralyze6.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
-		new_paralyze7.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
-		new_paralyze8.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
-		new_paralyze9.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
-		new_paralyze10.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
-		new_paralyze11.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
-		new_paralyze12.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
-		new_paralyze.rotation = self.rotation
-		new_paralyze2.rotation = self.rotation+30
-		new_paralyze3.rotation = self.rotation+60
-		new_paralyze4.rotation = self.rotation+90
-		new_paralyze5.rotation = self.rotation+120
-		new_paralyze6.rotation = self.rotation+150
-		new_paralyze7.rotation = self.rotation+180
-		new_paralyze8.rotation = self.rotation+210
-		new_paralyze9.rotation = self.rotation+240
-		new_paralyze10.rotation = self.rotation+270
-		new_paralyze11.rotation = self.rotation+300
-		new_paralyze12.rotation = self.rotation+330
-		$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze) 
-		$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze2) 
-		$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze3) 
-		$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze4) 
-		$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze5) 
-		$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze6) 
-		$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze7) 
-		$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze8) 
-		$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze9) 
-		$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze10) 
-		$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze11) 
-		$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze12) 
-		
+ 
+func shoot2():
+	const PARALYZE = preload("res://scenes/paralyze.tscn")
+	var new_paralyze = PARALYZE.instantiate()
+	var new_paralyze2 = PARALYZE.instantiate()
+	var new_paralyze3 = PARALYZE.instantiate()
+	var new_paralyze4 = PARALYZE.instantiate()
+	var new_paralyze5 = PARALYZE.instantiate()
+	var new_paralyze6 = PARALYZE.instantiate()
+	var new_paralyze7 = PARALYZE.instantiate()
+	var new_paralyze8 = PARALYZE.instantiate()
+	var new_paralyze9 = PARALYZE.instantiate()
+	var new_paralyze10 = PARALYZE.instantiate()
+	var new_paralyze11 = PARALYZE.instantiate()
+	var new_paralyze12 = PARALYZE.instantiate()
+	new_paralyze.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position 
+	new_paralyze2.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_paralyze3.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_paralyze4.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_paralyze5.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_paralyze6.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_paralyze7.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_paralyze8.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_paralyze9.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_paralyze10.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_paralyze11.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_paralyze12.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_paralyze.rotation = self.rotation
+	new_paralyze2.rotation = self.rotation+30
+	new_paralyze3.rotation = self.rotation+60
+	new_paralyze4.rotation = self.rotation+90
+	new_paralyze5.rotation = self.rotation+120
+	new_paralyze6.rotation = self.rotation+150
+	new_paralyze7.rotation = self.rotation+180
+	new_paralyze8.rotation = self.rotation+210
+	new_paralyze9.rotation = self.rotation+240
+	new_paralyze10.rotation = self.rotation+270
+	new_paralyze11.rotation = self.rotation+300
+	new_paralyze12.rotation = self.rotation+330
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze2) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze3) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze4) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze5) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze6) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze7) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze8) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze9) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze10) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze11) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_paralyze12)
+	can_shoot=true
+	
+func shoot3():
+	const SLASH = preload("res://scenes/slash.tscn")
+	var new_slash = SLASH.instantiate()
+	var new_slash2 = SLASH.instantiate()
+	var new_slash3 = SLASH.instantiate()
+	var new_slash4 = SLASH.instantiate()
+	var new_slash5 = SLASH.instantiate()
+	var new_slash6 = SLASH.instantiate()
+	var new_slash7 = SLASH.instantiate()
+	var new_slash8 = SLASH.instantiate()
+	var new_slash9 = SLASH.instantiate()
+	var new_slash10 = SLASH.instantiate()
+	var new_slash11 = SLASH.instantiate()
+	var new_slash12 = SLASH.instantiate()
+	var new_slash13 = SLASH.instantiate()
+	var new_slash14 = SLASH.instantiate()
+	var new_slash15 = SLASH.instantiate()
+	var new_slash16 = SLASH.instantiate()
+	var new_slash17 = SLASH.instantiate()
+	var new_slash18 = SLASH.instantiate()
+	var new_slash19 = SLASH.instantiate()
+	var new_slash20 = SLASH.instantiate()
+	var new_slash21 = SLASH.instantiate()
+	var new_slash22 = SLASH.instantiate()
+	var new_slash23 = SLASH.instantiate()
+	var new_slash24 = SLASH.instantiate()
+	new_slash.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position 
+	new_slash2.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash3.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash4.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash5.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash6.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash7.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash8.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash9.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash10.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash11.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash12.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash13.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash14.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash15.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash16.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash17.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash18.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash19.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash20.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash21.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash22.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash23.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash24.global_position = $Point/AnimatedSprite2D/ShootingPoint.global_position
+	new_slash.rotation = self.rotation+15
+	new_slash2.rotation = self.rotation+45
+	new_slash3.rotation = self.rotation-45
+	new_slash4.rotation = self.rotation+30
+	new_slash5.rotation = self.rotation-15
+	new_slash6.rotation = self.rotation-30
+	new_slash7.rotation = self.rotation+60
+	new_slash8.rotation = self.rotation-60
+	new_slash9.rotation = self.rotation+75
+	new_slash10.rotation = self.rotation-75
+	new_slash11.rotation = self.rotation+90
+	new_slash12.rotation = self.rotation-90
+	new_slash13.rotation = self.rotation+105
+	new_slash14.rotation = self.rotation-105
+	new_slash15.rotation = self.rotation+120
+	new_slash16.rotation = self.rotation-120
+	new_slash17.rotation = self.rotation+135
+	new_slash18.rotation = self.rotation-135
+	new_slash19.rotation = self.rotation+150
+	new_slash20.rotation = self.rotation-150
+	new_slash21.rotation = self.rotation+165
+	new_slash22.rotation = self.rotation-165
+	new_slash23.rotation = self.rotation+180
+	new_slash24.rotation = self.rotation-180
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash2) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash3) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash4) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash5) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash6)
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash7) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash8) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash9) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash10) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash11) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash12) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash13) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash14) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash15) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash16) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash17) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash18) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash19) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash20) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash21) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash22) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash23) 
+	$Point/AnimatedSprite2D/ShootingPoint.add_child(new_slash24)
 
 func _on_attack_timer_timeout() -> void:
 	shoot()
 
-func _on_attack_timer_2_timeout() -> void:
-	shoot()
+#func _on_attack_timer_2_timeout() -> void:
+	#shoot2()
+
+#func _on_attack_timer_3_timeout() -> void:
+	#shoot3()
--- scripts/oryx.gd
+++ scripts/oryx.gd
@@ -3,33 +3,101 @@ extends CharacterBody2D
 class_name Oryx
 
 signal health_changed
-var maxHealth=500
-var health=500
+signal health_depleted
+var maxHealth=700
+var health=700
 var player
+var has_executed: bool = false
+var is_first_iteration = true
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var aim = $Aim
+@onready var move_timer: Timer = $MoveTimer
+@onready var move_timer2: Timer = $MoveTimer2
+@export var jump_speed: float = 2500.0  # Adjust as needed
+@export var paralyze_count: int = 24
+@export var slash_count: int = 72
+@export var slash_repeats: int = 10
+@export var slash_interval: float = 0.1
+@export var delay_between_phases: float = 2.5
+
+var is_jumping: bool = false
 func _ready():
 	player = get_node("/root/Game/Player")
 
 func _physics_process(delta: float) -> void:
-	var direction = global_position.direction_to(player.global_position)
-	if health>maxHealth/2:
-		velocity = direction * 25
-	else:
-		velocity = direction * 0
-	move_and_slide()
+	if !has_executed:
+		var direction = global_position.direction_to(player.global_position)
+		if health>maxHealth/2:
+			velocity = direction * 25
+		else:
+			has_executed=true
+			start_process()
+			
+		move_and_slide()
+	
+func start_process() -> void:
+	if is_first_iteration:
+		await delay(1.0)  # Delay only before the first jump
+		is_first_iteration = false  # Ensure this delay happens only once
+	while health>0:
+		await jump_to_player()
+		await create_paralyze()
+		await create_slashes()
+		await delay(delay_between_phases)
+		
+func jump_to_player() -> void:
+	if player:
+		is_jumping = true
+		var direction = (player.global_position - global_position).normalized()
+		global_position += direction * jump_speed * 0.016
+		is_jumping = false
+	await delay(0.01)  # Brief pause after jump
 
+func create_paralyze() -> void:
+	var shooting_point = aim.get_node("Point/AnimatedSprite2D/ShootingPoint")
+	const PARALYZE = preload("res://scenes/paralyze.tscn")
+	if shooting_point:
+		for i in range(paralyze_count):
+			var angle = deg_to_rad(i * 15)  # Calculate angle in radians (30 degrees apart)
+			var paralyze_instance = PARALYZE.instantiate()
+			# Position and rotate the paralyze instance
+			paralyze_instance.global_position = shooting_point.global_position
+			paralyze_instance.rotation = angle
+			# Add it as a child of aim
+			aim.add_child(paralyze_instance)
+		
+	await delay(3.0)  # Wait for 3 seconds after creating paralyze instances
+
+func create_slashes() -> void:
+	var shooting_point = aim.get_node("Point/AnimatedSprite2D/ShootingPoint")
+	const SLASH = preload("res://scenes/slash.tscn")
+	if shooting_point:
+		for repeat in range(slash_repeats):
+			for i in range(slash_count):
+				var angle = deg_to_rad(i * 5)  # Calculate angle in radians (15 degrees apart)
+				var slash_instance = SLASH.instantiate()
+					
+				# Position and rotate the slash instance
+				slash_instance.global_position = shooting_point.global_position
+				slash_instance.rotation = angle
+					
+				# Add it as a child of aim
+				aim.add_child(slash_instance)
+				
+			await delay(slash_interval)  # Wait between slash groups
+
+func delay(seconds: float) -> void:
+	await get_tree().create_timer(seconds).timeout
+	
 func take_damage():
 	health-=10
 	health_changed.emit()
 	animation_player.play("oryx_hit")
 	if health<=0:
+		health_depleted.emit()
 		animation_player.play("oryx_death")
 		$TextureProgressBar.queue_free()
-		aim.queue_free()
-		animation_player.connect("animation_finished", Callable(self, "_on_animation_finished"))
-		
-
+		animation_player.connect("animation_finished", Callable(self, "_on_animation_finished"))	
 
 func _on_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "oryx_death":
